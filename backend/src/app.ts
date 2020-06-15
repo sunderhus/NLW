da9c191fde26ea6160a987c9b26e127f2276d841
matheus <matheus.sunderhus@gmail.com>
2020-06-15 16:13:35 -0300

    - Finaliza validações do backend na criação de um ponto de coleta.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,15 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import path from 'path';
-
+import {errors} from 'celebrate';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-app.use(routes);
+
+app.use(errors());
 
 export default app;
--- backend/src/routes/points.routes.ts
+++ backend/src/routes/points.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import {celebrate, Joi} from 'celebrate';
 import PointsController from '../controllers/PointsController';
+
 import multer from 'multer';
+
 import multerConfig  from '../config/index'
 
 const pointsRouter = Router();
@@ -11,6 +14,23 @@ const pointsController = new PointsController();
 
 pointsRouter.get('/:id', pointsController.show);
 pointsRouter.get('/', pointsController.index);
-pointsRouter.post('/',upload.single('image'), pointsController.create);
+pointsRouter.post(
+  '/',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      whatsapp: Joi.number().required(),
+      lat: Joi.number().required(),
+      lng:Joi.number().required(),
+      city:Joi.string().required(),
+      uf:Joi.string().max(2).min(2).required(),
+      items: Joi.string().required(),
+    })
+  },{
+    abortEarly:false,
+  }),
+  pointsController.create);
 
 export default pointsRouter;
